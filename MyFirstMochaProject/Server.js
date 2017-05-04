39c49efd4f3473e37952d6e1a0b7a275775bf124
lukamarijn <lukabrinkmannn@gmail.com>
2017-05-04 11:48:49 +0200

    Opdracht 2 practicum 2

--- MyFirstMochaProject/Server.js
+++ MyFirstMochaProject/Server.js
@@ -5,10 +5,11 @@
 
 var http = require('http');
 var express = require('express');
-
+var config = require('./tsconfig.json')
 var app = express();
 
-
+app.set('PORT', config.webPort);
+var port = process.env.PORT || app.get('PORT');
 app.all('*', function (req, res, next) {
     console.log( req.method + "" + req.url) ;
     next();
@@ -20,6 +21,6 @@ app.get('/api/v1/hello', function(req, res, next) {
 });
 
 
-app.listen(8080, function() {
+app.listen(port, function() {
     console.log("The magic happens at http://localhost: 8080");
 });
